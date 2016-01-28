e268b197f95241d420d14bac1c4929024496ed55
Dennis Vermeulen <dennis@wecreatepixels.nl>
2016-01-28 01:07:42 +0100

    Fix url deprecation warnings

--- brookie/urls.py
+++ brookie/urls.py
@@ -1,20 +1,21 @@
-from django.conf.urls import *
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'brookie.views',
+from . import views
+
+urlpatterns = [
     url(r'^invoice/(?P<pk>[\d]+)/pdf/$',
-        'generate_invoice',
+        views.generate_invoice,
         name='generate-invoice'),
 
     url(r'^invoice/(?P<pk>[\d]+)/download/$',
-        'view_invoice',
+        views.view_invoice,
         name='view-invoice'),
 
     url(r'^quote/(?P<pk>[-\d]+)/pdf/$',
-        'generate_quote',
+        views.generate_quote,
         name='generate-quote'),
 
     url(r'^quote/(?P<pk>[-\d]+)/invoice/$',
-        'quote_to_invoice',
+        views.quote_to_invoice,
         name='quote-to-invoice'),
-)
+]
